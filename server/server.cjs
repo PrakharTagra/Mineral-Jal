5b85025cd5ab6fb62218a2eab56ae8bfc44e68ff
PrakharTagra <prakhartagra16@gmail.com>
2026-02-08 00:02:40 +0530

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -45,6 +45,8 @@ async function start() {
     }
   });
 
-  app.listen(5000, () => console.log("Server running on 5000"));
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log("Server running on", PORT));
+
 }
 start();
