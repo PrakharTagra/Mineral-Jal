4eee59b239564bac3ab17a0f7de71b482d8b4bce
PrakharTagra <prakhartagra16@gmail.com>
2026-02-07 23:59:45 +0530

    url

--- Login/login.jsx
+++ Login/login.jsx
@@ -9,13 +9,15 @@ export default function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/login`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+
 
     const data = await res.json();
 
--- server/server.cjs
+++ server/server.cjs
@@ -48,5 +48,3 @@ async function start() {
   app.listen(5000, () => console.log("Server running on 5000"));
 }
 start();
-
-
