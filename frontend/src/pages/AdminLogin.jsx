ebd7f3d9fc59c0d06399ddcec4f8ebcc3e66876e
PrakharTagra <prakhartagra16@gmail.com>
2026-02-13 18:58:05 +0530

    re

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -10,7 +10,7 @@ export default function AdminLogin() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:3000/api/admin/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/admin/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
